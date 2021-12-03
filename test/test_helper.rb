a4e610a95e866122edcd167609b60081e094fa80
Alina <alya.petsko@gmail.com>
2021-12-03 14:38:50 +0200

    Finish log in/log out

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -10,4 +10,25 @@ class ActiveSupport::TestCase
   fixtures :all
 
   # Add more helper methods to be used by all tests here...
+  def is_logged_in?
+    !session[:user_id].nil?
+  end
+
+  def log_in_as(user, options = {})
+    session = {}
+    session[:user_id] = nil
+
+    password    = options[:password]    || 'password'
+    remember_me = options[:remember_me] || '1'
+    
+    session_params = {
+      session: { 
+        email: user.email,
+        password: password,
+        remember_me: remember_me 
+      }
+    }
+
+    post login_path(session_params)
+  end   
 end 
