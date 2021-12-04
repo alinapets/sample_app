b955547add1d1c7c223d7b5ff9abed38045b7ec6
Alina <alya.petsko@gmail.com>
2021-12-04 17:57:58 +0200

    Finish user edit, update, index, and destroy actions

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -9,6 +9,11 @@ module SessionsHelper
     cookies.permanent[:remember_token] = user.remember_token
   end
 
+  # Возвращает true, если заданный пользователь является текущим.
+  def current_user?(user)
+    user == current_user
+  end
+  
   # Возвращает текущего вошедшего пользователя (если есть).
   def current_user
     if (user_id = session[:user_id])
@@ -38,4 +43,15 @@ module SessionsHelper
     @current_user = nil
   end
 
+   # Перенаправляет к сохраненному расположению (или по умолчанию).
+   def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end
+
+  # Сохраняет запрошенный URL.
+  def store_location
+    session[:forwarding_url] = request.url if request.get?
+  end
 end
+
